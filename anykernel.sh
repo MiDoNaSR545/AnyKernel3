1e2bfc9c7fcf201038b699aed27276d2b172f380
kanger-303 <kanger303@gmail.com>
2025-05-14 18:50:48 +0700

    Anykernel3: Update Banner
    Signed-off-by: kanger-303 <kanger303@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernol developer @Zyusz
+kernel.string=Kernel By @AmbaTaull
 do.devicecheck=1
 do.modules=0
 do.systemless=1
